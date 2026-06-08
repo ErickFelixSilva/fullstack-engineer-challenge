b1134ccc9b1a979c646bef5f23fb00cbe4ed2b99
Erick Felix da Silva <erickfelix16@gmail.com>
2026-06-08 18:35:31 -0300

    Backend fully functional.

--- src/main/java/com/felix/fullstackengineerchallenge/controllers/TodoController.java
+++ src/main/java/com/felix/fullstackengineerchallenge/controllers/TodoController.java
@@ -3,14 +3,12 @@ package com.felix.fullstackengineerchallenge.controllers;
 import com.felix.fullstackengineerchallenge.domain.SaveTodoCommand;
 import com.felix.fullstackengineerchallenge.domain.Todo;
 import com.felix.fullstackengineerchallenge.service.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class TodoController {
 
     private final TodoService todoService;
